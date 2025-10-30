8790d2b3fa6b658e8ee236ad3779e821e50e0490
Jendrik <108542566+jenuni@users.noreply.github.com>
2025-10-30 17:25:29 +0100

    Create env.py

---
+++ src/env.py
@@ -0,0 +1,6 @@
+import os
+
+DATA_DIR = "./data"
+GRAPH_DIR = os.path.join(DATA_DIR, "wikispeedia_paths-and-graph")
+TEXT_DIR = os.path.join(DATA_DIR, "plaintext_articles")
+HTML_DIR = os.path.join(DATA_DIR, "wikispeedia_articles_html", "wpcd")
